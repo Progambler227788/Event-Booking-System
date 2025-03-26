f5ee1c68499fc2794e14873a8e5260e5684c6371
talha atif <talhaatif573@gmail.com>
2025-03-26 19:50:33 +0500

    Configuring Sockets

--- src/main/java/com/talhaatif/ticketbook/config/WebSocketConfig.java
+++ src/main/java/com/talhaatif/ticketbook/config/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint("/ws")  .setAllowedOriginPatterns("*")  // Changed from setAllowedOrigins
+                .withSockJS();  // Keep this for browser clients
     }
 }
